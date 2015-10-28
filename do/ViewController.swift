f2e397bd5dee34fba56ec54303e63d27398fac35
yhozumi <yhozumi@student.cscc.edu>
2015-10-28 14:32:44 -0400

    Initial Commit

---
+++ do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  do
+//
+//  Created by Yusuke Hozumi on 10/28/15.
+//  Copyright © 2015 Yusuke Hozumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
