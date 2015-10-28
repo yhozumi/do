47cdd178855960ea8c2f92eff238e9e07d5e67e5
yhozumi <yhozumi@student.cscc.edu>
2015-10-28 14:58:18 -0400

    Added background image to the project and changed the status bar content style to LightContent. Also Added new LoginViewController.

--- do/LoginViewController.swift
+++ do/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  do
 //
 //  Created by Yusuke Hozumi on 10/28/15.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
