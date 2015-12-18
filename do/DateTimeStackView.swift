99d8fd1693fb9abb97a0b5c6c9035aae907417a3
yhozumi <yusuke.hozumi@icloud.com>
2015-12-18 17:47:52 -0500

    Added a method to help animate the positioning change of the minutesView. However there's a bug where the minutesView will duplicate instead of moving from one position to another.

--- do/DateTimeStackView.swift
+++ do/DateTimeStackView.swift
@@ -37,6 +37,7 @@ class DateTimeStackView: UIStackView {
         return formatter
     }()
     
+    //This method will layout and position the minutesView on the clockView.
     private func setUpMinutesView() {
         self.minutesView = UIView(frame: CGRect(x: 0, y: 0, width: clockView.bounds.width / 3, height: clockView.bounds.height / 3))
         self.minutesView.backgroundColor = UIColor.whiteColor()
@@ -49,6 +50,11 @@ class DateTimeStackView: UIStackView {
         self.minutesView.center = path.currentPoint
         self.addSubview(minutesView)
         
+        setUpMinutesLabel()
+    }
+    
+    //helper method to instantiate minutesLabel
+    private func setUpMinutesLabel() {
         minutesLabel = UILabel(frame: CGRect.zero)
         minutesLabel.textColor = UIColor.whiteLightGreyColor()
         minutesLabel.text = timeTuple!.1
@@ -57,15 +63,26 @@ class DateTimeStackView: UIStackView {
         self.addSubview(minutesLabel)
     }
     
-    
     func timerCalled() {
         date = NSDate()
         timeTuple = createHoursAndMinutesFromDate(date)
         updateTimeLabels(timeTuple!)
     }
     
+    //Helper method that's called everytime the timer is fired
     private func updateTimeLabels(time: (String, String)) {
         hourLabel.text = time.0
+        guard let minutesLabel = minutesLabel else { return }
+        minutesLabel.text = time.1
+        minutesLabel.sizeToFit()
+        
+        if Int(time.1)! % 5 == 0 {
+            let endAngle = MinutesHandIdentifier.configureRadiansWithMinutes(Int(time.1)!)
+            let path = UIBezierPath(arcCenter: self.clockView.center, radius: self.clockView.bounds.width / 2, startAngle: CGFloat(0), endAngle: endAngle, clockwise: true)
+            UIView.animateWithDuration(0.5, animations: {
+                self.minutesView.center = path.currentPoint
+                }, completion: nil)
+        }
     }
     
     private func updateLabels() {
@@ -73,6 +90,7 @@ class DateTimeStackView: UIStackView {
         dayLabel.text = dayFormatter.stringFromDate(date)
     }
     
+    //Helper method to create time tuple to display hour and minutes to the user.
     private func createHoursAndMinutesFromDate(date: NSDate) -> (String, String) {
         let calendar = NSCalendar.currentCalendar()
         let time = calendar.components([.Hour, .Minute], fromDate: date)
@@ -88,11 +106,9 @@ class DateTimeStackView: UIStackView {
         if let timeTuple = timeTuple {
             updateTimeLabels(timeTuple)
         }
-        
         clockView.layer.cornerRadius = clockView.frame.width / 2
         
         timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("timerCalled"), userInfo: nil, repeats: true)
-        
     }
     
     override func layoutSubviews() {
@@ -102,7 +118,7 @@ class DateTimeStackView: UIStackView {
     
     override func removeFromSuperview() {
         super.removeFromSuperview()
-        timer?.invalidate()
+        timer?.invalidate() //invalidates the timer so that timer doesn't keep this view in memory and cause memory leak
     }
     
     deinit {
